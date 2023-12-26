1e313714f6db3921ab48352b291d215339f768a9
AdityaDwivediAtGit <dwivedias_1@rknec.edu>
2023-12-26 23:08:33 +0530

    Added Transform and Load + minor changes in Extract
    Replacements done in above extract code:
    
    - fixed headers of db dataframe. `desc[0].replace("_"," ")`
    - added index as header in mystry df `output_file.write("index")`
    - replaced header `population_data_xml_df` in `header[0] = "Country Name"`
    - Lot of cleaning required in `projects` (SKIPPING THIS CLEANING AND DIRECTLY PROCEED TO LOADING)
    - Tried getting jar file for jdbc connection (downloaded it and added to spark session)

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 class DataCleaner:
     def __init__(self, debug= False):
         self.debug = debug
-        if self.debug: print("\tDEBUG MODE ON")
+        if self.debug: print("\t ---- DEBUG MODE ON ----")
         self.cleaning_folder = "cleaned_files"
         os.makedirs(self.cleaning_folder, exist_ok=True)
 
@@ -25,6 +25,7 @@ class DataCleaner:
 
     def resolve_encoding(self, input_path, output_path, encoding_from, encoding_to):
         with open(input_path, "r", encoding=encoding_from) as input_file, open(output_path, "w", encoding=encoding_to) as output_file:
+            output_file.write("index")
             output_file.write(input_file.read())
         if self.debug: print(f"Encoding of {input_path} changed from {encoding_from} to {encoding_to}")
 
@@ -35,6 +36,7 @@ class DataCleaner:
         with open(csv_file_path, 'w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
             header = [field.attrib['name'] for field in root.find('.//record').iter('field')]
+            header[0] = "Country Name"   ######## .replaced "Country or Area" to "Country Name" in header
             csv_writer.writerow(header)
 
             for record in root.findall('.//record'):
@@ -53,7 +55,7 @@ class DataCleaner:
         csv_file_path = 'cleaned_files/cleaned_'+tablename+"_db.csv"
         with open(csv_file_path, 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
-            csvwriter.writerow([desc[0] for desc in cursor.description])
+            csvwriter.writerow([desc[0].replace("_"," ") for desc in cursor.description])
             csvwriter.writerows(data)
 
         conn.close()
@@ -62,13 +64,107 @@ class DataCleaner:
     def spark_read_csv(self, file_path): return spark.read.csv(file_path, header=True, inferSchema=True)
     def spark_read_json(self, file_path): return spark.read.json(file_path)
 
+    def merge_dfs (self, *dfs):
+        combined_df = dfs[1]
+        for df in dfs[1:]:
+            combined_df = combined_df.unionByName(df, allowMissingColumns = True).distinct()
+        if debug: print("combined ", dfs, "dataframes")
+        return combined_df
+    
+    def showall(self):
+        try:
+            print("\telectricity_access_percent_df.show()")
+            electricity_access_percent_df.show()
+
+            print("\tgdp_data_df.show()")
+            gdp_data_df.show()
+
+            print("\tmystery_df.show()")
+            mystery_df.show()
+
+            print("\tpopulation_data_csv_df.show()")
+            population_data_csv_df.show()
+
+            print("\tpopulation_data_db_df.show()")
+            population_data_db_df.show()
+
+            print("\tpopulation_data_json_df.show()")
+            population_data_json_df.show()
+
+            print("\tpopulation_data_xml_df.show()")
+            population_data_xml_df.show()
+
+            print("\tprojects_data_xml_df.show()")
+            projects_data_xml_df.show()
+
+            print("\trural_population_percent_df.show()")
+            rural_population_percent_df.show()
+
+            print("\tpopulation_combined_df.show()")
+            population_combined_df.show()
+        except: pass
+    
+    def load(self, output_file):
+        import sqlite3
+        from pyspark.sql.types import StringType, IntegerType, DoubleType
+
+        projects_data_df = projects_data_xml_df
+
+        # Function to create a table based on the schema of a DataFrame
+        def create_table(cursor, table_name, df):
+            type_mapping = {
+                StringType(): 'TEXT',
+                IntegerType(): 'INTEGER',
+                DoubleType(): 'REAL'
+            }
+
+            columns_and_types = [(col.name, type_mapping.get(col.dataType, 'TEXT')) for col in df.schema]
+            columns_definition = ', '.join([f'"{col}" {data_type}' for col, data_type in columns_and_types])
+            create_table_query = f'CREATE TABLE IF NOT EXISTS {table_name} ({columns_definition})'
+            
+            if debug: print(f'Generated query: {create_table_query}')  # Print the query for debugging purposes
+            
+            cursor.execute(create_table_query)
+
+
+        # Function to insert data into the table
+        def insert_data(cursor, table_name, df):
+            insert_data_query = f'INSERT INTO {table_name} VALUES ({", ".join(["?"] * len(df.columns))})'
+            if debug: print(f'Generated query: {insert_data_query}')  # Print the query for debugging purposes
+            for row in df.collect():
+                cursor.execute(insert_data_query, tuple(row))
+
+        # Connect to SQLite and create tables
+        connection = sqlite3.connect(output_file)
+        cursor = connection.cursor()
+
+        create_table(cursor, 'population_data', population_combined_df)
+        create_table(cursor, 'gdp_data', gdp_data_df)
+        create_table(cursor, 'electricity_access_percent', electricity_access_percent_df)
+        create_table(cursor, 'rural_population_percent', rural_population_percent_df)
+        create_table(cursor, 'mystery', mystery_df)
+        create_table(cursor, 'projects_data', projects_data_df)
+
+        # Insert data into tables
+        insert_data(cursor, 'population_data', population_combined_df)
+        insert_data(cursor, 'gdp_data', gdp_data_df)
+        insert_data(cursor, 'electricity_access_percent', electricity_access_percent_df)
+        insert_data(cursor, 'rural_population_percent', rural_population_percent_df)
+        insert_data(cursor, 'mystery', mystery_df)
+        insert_data(cursor, 'projects_data', projects_data_df)
+
+        # Commit the changes and close the connection
+        connection.commit()
+        connection.close()
+
 # Main
 if __name__ == "__main__":
 
     debug = True                    ##### YOU CAN SWITCH DEBUGGER HERE #####
-    
+
     cleaner = DataCleaner(debug = debug)
 
+    if debug: print("\t---- EXTRACTION INITIATED ----\t")
     # CSV (4 line removal)
     csv_files = ["electricity_access_percent.csv", "gdp_data.csv", "population_data.csv", 'rural_population_percent.csv']
     for csv_file in csv_files:
@@ -94,7 +190,7 @@ if __name__ == "__main__":
 
     population_data_csv_df = cleaner.spark_read_csv('cleaned_files/cleaned_population_data.csv')
 
-    # Convert DB to CSV 
+    # Convert DB to CSV
     cleaner.convert_db_to_csv(filename = 'population_data.db', tablename = "population_data")
     population_data_db_df = cleaner.spark_read_csv('cleaned_files/cleaned_population_data_db.csv')
 
@@ -107,3 +203,17 @@ if __name__ == "__main__":
 
     # Read other CSV files
     projects_data_xml_df = cleaner.spark_read_csv('projects_data.csv')
+
+    if debug: print("\t---- EXTRACT COMPLETED \t PROCEDING WITH TRANSFORMATION ----\t")
+
+    population_combined_df = cleaner.merge_dfs(population_data_csv_df, population_data_db_df, population_data_json_df, population_data_xml_df)
+
+    if debug: print("\t---- TRANSFORMATION COMPLETED \t PROCEDING WITH LOADING ----\t")
+    cleaner.load(output_file="combined_data_db.sqlite")
+
+    # END SPARK SESSION
+    if debug: 
+        print("\t---- LOADING FINISHED ----\t")
+        cleaner.showall()
+    
+    spark.stop()
